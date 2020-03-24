0802e083531c962563076670c425629e7a2036e6
Lourran <achimidd@gmail.com>
2020-03-24 10:51:36 -0300

    teste

--- src/index.js
+++ src/index.js
@@ -137,6 +137,6 @@ function bytesToSize(bytes) {
 }   
 
 const url = 'magnet:?xt=urn:btih:DMBJ7EIFGZZFIC6O5KCMROPDSVB22OUS&tr=http://nyaa.tracker.wf:7777/announce&tr=udp://tracker.coppersurfer.tk:6969/announce&tr=udp://tracker.internetwarriors.net:1337/announce&tr=udp://tracker.leechersparadise.org:6969/announce&tr=udp://tracker.opentrackr.org:1337/announce&tr=udp://open.stealth.si:80/announce&tr=udp://p4p.arenabg.com:1337/announce&tr=udp://mgtracker.org:6969/announce&tr=udp://tracker.tiny-vps.com:6969/announce&tr=udp://peerfect.org:6969/announce&tr=http://share.camoe.cn:8080/announce&tr=http://t.nyaatracker.com:80/announce&tr=https://open.kickasstracker.com:443/announce'
-// startDownload(url)
+startDownload(url)
 
 startServer()
